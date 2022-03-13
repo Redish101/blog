45b45b523235124e25f71acd0afb2145668de875
Redish101 <jiayunluo@outlook.com>
2022-03-13 14:59:44 +0800

    sss

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,12 +6,12 @@ module.exports = {
     'type-enum': [2, 'always', [
       'upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert'
      ]],
-    'type-case': [0],
-    'type-empty': [0],
-    'scope-empty': [0],
-    'scope-case': [0],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72]
+    'type-case': [2],
+    'type-empty': [2],
+    'scope-empty': [2],
+    'scope-case': [2],
+    'subject-full-stop': [2, 'never'],
+    'subject-case': [2, 'never'],
+    'header-max-length': [2, 'always', 72]
   }
 };
