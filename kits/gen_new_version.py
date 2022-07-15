82eb4abf8e00d5d58926b017c511ed6ffa2df691
Redish101 <jiayunluo@outlook.com>
2022-07-15 12:23:52 +0800

    fix: 修复了自动更新版本的错误

--- kits/gen_new_version.py
+++ kits/gen_new_version.py
@@ -14,7 +14,11 @@ with open("package.json",'w',encoding='utf-8') as f:
 with open(r"cw/config.yaml",'r',encoding='utf-8') as y:
     cw = yaml.safe_load(y)
 
-cw['version'] = '\'&\''+new_version
+a = cw['catch_rules']
+b = a[0]
+c = b['transform_rules']
+d = c[5]
+d['replace'] = 'blogcdn/redish101-blog@'+new_version+'/public'
 
 with open(r"cw/config.yaml",'w',encoding='utf-8') as y:
     yaml.safe_dump(cw, y)
