a9a11e89a4e23c38144860330efd331071e12d88
Redish101 <Redish101@users.noreply.github.com>
2022-10-08 08:08:58 +0000

    deploy: 4e7d25ffa934f19482090c7b4150242c8b316280

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@
 const cache_name = 'Redish101BlogHelper' // 缓存名称
 let cachelist = [
     '/helper/error.html',
-    '/helper/offline.html'
+    '/helper/offline.html',
+    '/'
 ] //缓存列表
 
 // 监听器
@@ -35,7 +36,7 @@ const handle = async(req) => {
         return fetch(req.url.replace("http://cdn.jsdelivr.net", "https://cdn1.tianli0.top"))
     }
     return fetch(req.url).then(function (res) {
-        if (!res) { throw 'error' } //1
+        xsa
         return caches.open(cache_name).then(function (cache) {
             cache.delete(req);
             cache.put(req, res.clone());
